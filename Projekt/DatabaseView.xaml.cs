458089a740d4cfb78dbc7460862707d1d3be601b
SomaHam44 <somasolti@gmail.com>
2022-01-02 13:56:27 +0100

    Próbálgatások, tervezések, ötletelésék a nézethez

--- Projekt/DatabaseView.xaml.cs
+++ Projekt/DatabaseView.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Net.Http;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -19,9 +20,48 @@ namespace Projekt
     /// </summary>
     public partial class DatabaseView : Window
     {
+        static string Url = "http://quizion.hu/api/quizes";
         public DatabaseView()
         {
+            
             InitializeComponent();
+        }
+
+        private async Task listazas(String url)
+
+        {
+            using (var client = new HttpClient())
+            {
+                /*
+                Encoding enc = Encoding.ASCII;
+                string json = JsonConvert.SerializeObject(url);
+                var adat = new StringContent(json, Encoding.UTF8, "application/json");
+                //lbl_listaz.Text = json;
+                lbl_listaz.Text = url;
+
+                */
+                //var adata = new StringContent(answer, Encoding.ASCII, "application/json");
+
+
+                // működik de nem magyar kódolás
+                string answer = await client.GetStringAsync(url);
+                lbl_listaz.Text = answer[0].ToString();
+                lista.Add(answer);
+                lbl_listaz.Text = lista[0];
+
+
+
+
+
+
+            }
+
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
