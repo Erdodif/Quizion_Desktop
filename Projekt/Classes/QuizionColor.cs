89cfbad1b7e8ae8dac4527ba545a11c09cb40016
SomaHam44 <somasolti@gmail.com>
2022-03-19 14:43:19 +0100

    Szín osztály angolosítása

---
+++ Projekt/Classes/QuizionColor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Projekt
+{
+    enum QuizionColor
+    {
+        Primary = 0x50508E,
+        PrimaryVariant = 0x211A52,
+        Secondary = 0x7985C1,
+        SecondaryVariant = 0x5B6AB0,
+        OnSecondary = 0x4053A0,
+        OnPrimary = 0xE8E7F5,
+        Warning = 0xBA0100,
+        Alert = 0xBAA100,
+        Fine = 0x1CBA00,
+        Black = 0x000000,
+        White = 0xFFFFFF
+    }
+        
+
+}
+
+
+
+
+   
+
