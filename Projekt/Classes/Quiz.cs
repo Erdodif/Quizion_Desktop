e92527bdd1b550a8e12da098540743b66eb30aaa
SomaHam44 <somasolti@gmail.com>
2022-03-22 13:05:30 +0100

    Quiz osztály módosítása

--- Projekt/Classes/Quiz.cs
+++ Projekt/Classes/Quiz.cs
@@ -14,25 +14,26 @@ namespace Projekt
         private string header;
         private string description;
         private int active;
-        private int secondsPerQuiz;
+        private int seconds_per_quiz;
 
-        public int Id { get => id; set => id = value; }
-        public string Header { get => header; set => header = value; }
-        public string Description { get => description; set => description = value; }
-        public int Active { get => active; set => active = value; }
-        public int SecondsPerQuiz { get => secondsPerQuiz; set => secondsPerQuiz = value; }
-
-        public Quiz(int id, string header,  string description,int active, int secondsPerQuiz)
+        public Quiz(int id, string header, string description, int active, int seconds_per_quiz)
         {
             this.id = id;
             this.header = header;
             this.description = description;
             this.active = active;
-            this.secondsPerQuiz = secondsPerQuiz;
+            this.seconds_per_quiz = seconds_per_quiz;
         }
+
+        public int Id { get => id; set => id = value; }
+        public string Header { get => header; set => header = value; }
+        public string Description { get => description; set => description = value; }
+        public int Active { get => active; set => active = value; }
+        public int Seconds_per_quiz { get => seconds_per_quiz; set => seconds_per_quiz = value; }
+
         public override string ToString()
         {
-            return $"{id};{header};{description};{active};{secondsPerQuiz}";
+            return $"{id};{header};{description};{active};{seconds_per_quiz}";
         }
 
     }
